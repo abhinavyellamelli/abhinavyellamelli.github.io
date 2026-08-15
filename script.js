7d7332192ad2ad8ec580e1d266b80ff03858b618
Abhinav Yellamelli <abhinavyellamelli234@gmail.com>
2026-08-15 14:42:09 +0530

    Updated portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+// =========================================================
+// MOBILE MENU
+// =========================================================
+
+const menuIcon = document.getElementById("menu-icon");
+const navLinks = document.querySelector(".nav-links");
+
+menuIcon.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
