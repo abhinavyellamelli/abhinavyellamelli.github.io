ad247d01361374bafb9c481388ac4cb71f88adb7
Abhinav Yellamelli <abhinavyellamelli234@gmail.com>
2026-08-15 17:55:39 +0530

    Updated portfolio

--- script.js
+++ script.js
@@ -5,8 +5,13 @@
 const menuIcon = document.getElementById("menu-icon");
 const navLinks = document.querySelector(".nav-links");
 
-menuIcon.addEventListener("click", () => {
 
-    navLinks.classList.toggle("active");
+if (menuIcon && navLinks) {
 
-});
+    menuIcon.addEventListener("click", () => {
+
+        navLinks.classList.toggle("active");
+
+    });
+
+}
